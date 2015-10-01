a87f8661e067bdbf9f012c335123ea2739c8c900
zjkzjk <179226997@qq.com>
2015-10-01 17:40:52 +0800

    index更改

--- 前端/js/index.js
+++ 前端/js/index.js
@@ -1,3 +1,12 @@
-$(document).ready(function($) {
-	
+$(document).ready(function() {
+	$(".box_one>a").click(function() {
+		$(".display").toggle("slow");
+		$(".road_display").fadeOut("slow");
+		$(".main,.footer,.right").show();
+	});
+	$(".box_two>a").click(function() {
+		$(".road_display").toggle("slow");;
+		$(".display").fadeOut("slow");
+		$(".main,.footer,.right").toggle();
+	});
 });

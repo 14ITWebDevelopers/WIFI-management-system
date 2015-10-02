d7b3e669774c1a350414fee665be49bd3081d4a0
Waydrow <waydrow@163.com>
2015-10-02 15:56:28 +0800

    静态页面更新
    js效果更改优化, dom结构稍有变更

--- 前端/js/index.js
+++ 前端/js/index.js
@@ -1,12 +1,47 @@
 $(document).ready(function() {
-	$(".box_one>a").click(function() {
-		$(".display").toggle("slow");
-		$(".road_display").fadeOut("slow");
-		$(".main,.footer,.right").show();
+	$(".left_display").css('display', 'none');
+	var display = $(".display");
+	var leftDisplay = $(".left_display");
+	var boxOneA = $(".box_one>a");
+	var boxTwoA = $(".box_two>a");
+	boxOneA.click(function() {
+		// 背景蓝色切换
+		$(this).toggleClass('boxOneHover');
+		if(boxTwoA.hasClass('boxTwoHover'))
+			boxTwoA.removeClass('boxTwoHover');
+		// 判断下方div是否隐藏
+		if(! leftDisplay.is(':hidden')){
+			leftDisplay.hide();
+			$(".hide_details").toggleClass('hideTempAnother');
+		}
+		display.animate({width: 'toggle'});
 	});
-	$(".box_two>a").click(function() {
-		$(".road_display").toggle("slow");;
-		$(".display").fadeOut("slow");
-		$(".main,.footer,.right").toggle();
+	boxTwoA.click(function() {
+		$(this).toggleClass('boxTwoHover');
+		if(boxOneA.hasClass('boxOneHover'))
+			boxOneA.removeClass('boxOneHover');
+		if(!display.is(':hidden'))
+			display.hide();
+
+		$(".hide_details").toggleClass('hideTempAnother');
+		// $(".road_display>div:eq(1)").toggleClass('hideTemp');
+		leftDisplay.animate({width: 'toggle'});
 	});
+
+	var leftLi = $(".left_display ul li");
+	var rigthDiv = $(".hide_details");
+	var len = leftLi.length;
+
+	for(var i=0; i<len; i++){
+		leftLi[i].index = i;
+		leftLi[i].onclick = function(){
+			for(var n=0; n<len; n++){
+				leftLi[n].className = "";
+				rigthDiv[n].className = "hide_details" + " " + "hideTemp";
+			};
+			this.className = "showTemp";
+			rigthDiv[this.index].className = " " + "hide_details";
+		}
+	};
+	
 });
